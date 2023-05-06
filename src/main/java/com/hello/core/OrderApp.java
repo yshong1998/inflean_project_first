0461a439585d0996105d00a3125bc95b6f7a59f9
yshong1998 <yshong1998@gmail.com>
2023-05-06 21:01:00 +0900

    Refactor: 순수 자바 코드 대신 스프링 프레임 워크를 사용하도록 변경

--- src/main/java/com/hello/core/OrderApp.java
+++ src/main/java/com/hello/core/OrderApp.java
@@ -5,13 +5,19 @@ import com.hello.core.member.Member;
 import com.hello.core.member.MemberService;
 import com.hello.core.order.Order;
 import com.hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
