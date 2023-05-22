2f14d7f31c960e665796c35d99463e4c06b1aef8
yshong1998 <yshong1998@gmail.com>
2023-05-22 16:11:04 +0900

    다양한 의존관계 주입 방법

--- src/test/java/com/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/com/hello/core/scan/AutoAppConfigTest.java
@@ -1,12 +1,13 @@
 package com.hello.core.scan;
 
 import com.hello.core.AutoAppConfig;
+import com.hello.core.member.MemberRepository;
 import com.hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
+import com.hello.core.order.OrderServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class AutoAppConfigTest {
 
@@ -14,6 +15,10 @@ public class AutoAppConfigTest {
     void basicScan(){
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
         MemberService memberService = ac.getBean(MemberService.class);
-        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
