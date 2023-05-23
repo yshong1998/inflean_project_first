866a2a1f261d0ac66f4fe995e49f7850f7817ca9
yshong1998 <yshong1998@gmail.com>
2023-05-23 18:32:38 +0900

    옵션 처리

--- src/test/java/com/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/com/hello/core/singleton/ConfigurationSingletonTest.java
@@ -16,11 +16,14 @@ public class ConfigurationSingletonTest {
     @Test
     void configurationTest(){
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
         MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
         OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
         MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
-        MemberRepository memberRepository1 = memberService.getMemberRepository();;
-        MemberRepository memberRepository2 = orderService.getMemberRepository();;
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
         System.out.println("memberService -> memberRepository = " + memberRepository1);
         System.out.println("orderService -> memberRepository = " + memberRepository2);
         System.out.println("memberRepository = " + memberRepository);
