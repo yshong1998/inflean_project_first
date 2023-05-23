a4281f20acf4c6e633d2ea4c2bec9f27cacbc20c
yshong1998 <yshong1998@gmail.com>
2023-05-23 19:09:27 +0900

    롬복과 최신 트렌드

---
+++ src/main/java/com/hello/core/HelloLombok.java
@@ -0,0 +1,21 @@
+package com.hello.core;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("asdf");
+        String name = helloLombok.getName();
+        System.out.println("name = " + name);
+    }
+
+}
