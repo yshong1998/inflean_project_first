877d65e00b27e14ae8c1757543ab0e3a188bc6a1
yshong1998 <yshong1998@gmail.com>
2023-05-23 23:20:28 +0900

    @Autowired 필드명, @Qualifier, @Primary

--- src/main/java/com/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/com/hello/core/discount/FixDiscountPolicy.java
@@ -2,8 +2,10 @@ package com.hello.core.discount;
 
 import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 @Component
+@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;
--- src/main/java/com/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,12 @@ package com.hello.core.discount;
 
 import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
