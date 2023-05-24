b73e3cf9559aaa9dc249dae0540a40fd142cd6ce
yshong1998 <yshong1998@gmail.com>
2023-05-25 01:02:22 +0900

    스코프와 Provider

--- src/main/java/com/hello/core/web/LogDemoService.java
+++ src/main/java/com/hello/core/web/LogDemoService.java
@@ -2,15 +2,17 @@ package com.hello.core.web;
 
 import com.hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
-        System.out.println("service id = " + id);
+        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = " + id);
     }
 }
