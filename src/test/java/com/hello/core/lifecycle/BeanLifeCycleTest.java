141dc64472de6fb2a1df5e78bb1af35a8348a94e
yshong1998 <yshong1998@gmail.com>
2023-05-24 17:03:24 +0900

    애노테이션 @PostConstruct, @PreDestroy

--- src/test/java/com/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/com/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -1,7 +1,6 @@
 package com.hello.core.lifecycle;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +19,7 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
